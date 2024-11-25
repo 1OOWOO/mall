bba2e38cf99fe50d91ac094ef9a042491fc89198
GD <GD@DESKTOP-KVDOT51>
2024-11-25 11:11:08 +0900

    vo 주석추가

--- src/main/java/com/example/mall/vo/Address.java
+++ src/main/java/com/example/mall/vo/Address.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class Address {
-	private Integer addressNo; // pk
-	private String customerMail; // fk
+	private Integer addressNo; // primary key
+	private String customerMail; // foreign key
 	private String addressDetail;
 	private String updateDate;
-	private String createDate;
+	private String createDate; 
 }
