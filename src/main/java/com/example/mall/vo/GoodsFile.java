55ece105e5217b243646e200c29d569658b11a75
GOODEE <dkfla4992@gmail.com>
2024-11-25 12:28:01 +0900

    VO 추가

---
+++ src/main/java/com/example/mall/vo/GoodsFile.java
@@ -0,0 +1,16 @@
+package com.example.mall.vo;
+
+import lombok.Data;
+
+@Data
+public class GoodsFile {
+	private Integer goodsFileNo;
+	private Integer goodsNo;
+	private String goodsFileName;
+	private String goodsFileOrigin;
+	private String goodsFileExt;
+	private String goodsFileType;
+	private String goodsFileSize;
+	private String updateDate;
+	private String createDate;
+}
---
+++ src/main/java/com/example/mall/vo/Staff.java
@@ -0,0 +1,12 @@
+package com.example.mall.vo;
+
+import lombok.Data;
+
+@Data
+public class Staff {
+	private Integer staffNo;
+	private	String staffId;
+	private String staffPw;
+	private String updateDate;
+	private String createDate;
+}
