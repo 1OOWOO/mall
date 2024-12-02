24e7e8b9f9f3bcb8049ef2c06a3bfdec99aab2a2
jayoon-oh <dhwkdbs3@gmail.com>
2024-12-02 15:04:11 +0900

    오자윤) 장바구니 구현중

--- src/main/java/com/example/mall/mapper/GoodsMapper.java
+++ src/main/java/com/example/mall/mapper/GoodsMapper.java
@@ -10,7 +10,11 @@ import com.example.mall.vo.GoodsFile;
 
 @Mapper
 public interface GoodsMapper {
-// Author : 오아림
+	
+	// 오자윤 : 상품 정보 가져오기-> 장바구니
+	Map<String, Object> getGoodsById(String goodsNo);	
+	
+	// Author : 오아림
 	// /admin/addGoods
 	Integer insertGoods(Goods goods);
 	
