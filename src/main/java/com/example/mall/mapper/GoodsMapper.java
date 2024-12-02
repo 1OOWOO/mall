3516998b7a11489d4ab6a4b377652a37062ed218
dkfla <dkfla@DESKTOP-BR3V8C6>
2024-12-02 15:10:55 +0900

    Merge branch 'master' of https://github.com/1OOWOO/mall.git

--- src/main/java/com/example/mall/mapper/GoodsMapper.java
+++ src/main/java/com/example/mall/mapper/GoodsMapper.java
@@ -11,27 +11,32 @@ import com.example.mall.vo.GoodsFile;
 @Mapper
 public interface GoodsMapper {
 // Author : 오아림
+	
+	// 오자윤 : 상품 정보 가져오기-> 장바구니
+	Map<String, Object> getGoodsById(String goodsNo);	
+	
+	// Author : 오아림
 	// /admin/addGoods
 	Integer insertGoods(Goods goods);
-	
+
 	// /admin/goodsList
 	List<Map<String,Object>> selectGoodsList(Map<String,Object> map);
-	
+
 	// goodsList 페이징
 	Integer selectGoodsCount();
-	
+
 	// /admin/goodsOne
 	Map<String,Object> selectGoodsOne(Integer goodsNo);
-	
+
 	// /admin/goodsOne - 상품 수정
 	Integer updateGoods(Map<String,Object> map);
-	
+
 	// /customer/customerGoodsOne
 	Goods selectCustomerGoodsOne(Integer goodsNo);
-	
+
 	// /hello
 	List<Goods> selectAllGoods();
-	
+
 	// /customer/searchGoods
 	List<Map<String, Object>> selectGoodsByCategory(Integer categoryNo);
 }
